c2102aa961fe529b2406e1025cb523339ee8e1e4
jukaa57 <junioroliveira.1150@gmail.com>
2024-08-19 05:06:29 -0300

    repair prisma

--- actions.ts
+++ actions.ts
@@ -5,37 +5,37 @@ import { signInCredentials, signUpCredentials } from './shared/interfaces.d';
 export async function userExists(email: string) {
     const userExists = await prisma.users.findFirst({
         where: {
-            email: email
+          email: email
         }
     })
     return userExists ? true : false
 }
 
 export async function createAccout(data: signUpCredentials) {
-    await prisma.users.create({
-        data: {
-            username: data.username,
-            email: data.email,
-            hashpassword: data.password,
-            saltkey: data.saltKey as string,
-        },
+    await prisma.auth.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        hashpassword: data.password,
+        saltkey: data.saltKey as string,
+      },
     })
 }
 
 export async function getAccountFull(data: signInCredentials) {
-    const account = await prisma.users.findFirst({
+    const account = await prisma.auth.findFirst({
         where: {
-            email: data.email
+          email: data.email
         }
     })
     return account
 }
 
 export async function setAccessToken(data: signInCredentials, accessToken: string) {
-    await prisma.accessToken.create({
-        data: {
-            userId: data.id as string,
-            token: accessToken
-        },
+    await prisma.session.create({
+      data: {
+        userId: data.id as string,
+        token: accessToken
+      },
     })
 }
